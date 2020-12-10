cdf4fa541d725e67c11064f69a4995eb95f36391
ntawandae <56553042+ntawandae@users.noreply.github.com>
2020-12-10 23:08:53 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -32,21 +32,14 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
-
-
-   // typing text animation script
-    // var typed = new Typed(".typing", {
-    //     strings: ["Web-Developer", "Designer", "Freelancer","Photographer"],
-    //     typeSpeed: 180,
-    //     backSpeed: 90,
-    //     loop: true
-    // });
+//typing effect
     var typed = new Typed(".typing-2", {
         strings: ["Web-Developer", "Designer", "Freelancer","Photographer"],
         typeSpeed: 180,
         backSpeed: 90,
         loop: true
     });
+    //aos effect
     AOS.init({
     easing:'ease',
     duration:1800,
@@ -55,7 +48,7 @@ $(document).ready(function(){
 
 
 });
-
+//data filtering
  const filterContainer = document.querySelector(".portfolio-filter"),
  portfolioItems = document.querySelectorAll(".portfolio-item");
 
@@ -78,3 +71,32 @@ $(document).ready(function(){
    	 });
    }
  });
+//active testimonial
+    $('.testimonial-pics img').click(function() {
+      $(".testimonial-pics img").removeClass("active");
+      $(this).addClass("active");
+
+      $(".testimonial-text").removeClass("active");
+      $("#" + $(this).attr("alt")).addClass("active");
+    });
+//google api maps
+    function init_map() {
+      var myOptions = {
+        zoom: 14,
+        center: new google.maps.LatLng(19.075314480255834, 72.88153973865361),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
+      marker = new google.maps.Marker({
+        map: map,
+        position: new google.maps.LatLng(36.164129567567876, 120.4973639130302)
+      });
+      infowindow = new google.maps.InfoWindow({
+        content: '<strong>My Location</strong><br>Laoshan<br>'
+      });
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, marker);
+      });
+      infowindow.open(map, marker);
+    }
+    google.maps.event.addDomListener(window, 'load', init_map);
